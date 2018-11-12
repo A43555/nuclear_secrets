6875761f67074cfed5582f5f81cfae0a1399fb07
A43555 <43555@aluos.isel.ipl.pt>
2018-11-12 11:58:32 +0000

    lolis everywhere

---
+++ le_create.sql
@@ -0,0 +1,6 @@
+USE Conferencias_DB
+
+INSERT INTO Instituicao(nome, pais, morada) VALUES ('Instituicao 1', 'Portugal', 'Rua X')
+INSERT INTO Utilizador(email, nome, nome_instituicao) VALUES('matumbino@email.com', 'Matumbino Chota Grande', 'Instituicao 1')
+INSERT INTO Utilizador(email, nome, nome_instituicao) VALUES('tiburcio@email.com', 'Tiburcio Balde', 'Instituicao 1')
+INSERT INTO Utilizador(email, nome, nome_instituicao) VALUES('matumbina@email.com', 'Matumbina Pito Largo', 'Instituicao 1')
